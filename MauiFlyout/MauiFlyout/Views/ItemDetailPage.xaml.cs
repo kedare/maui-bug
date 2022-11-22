a74ade8067e982d011dfbe1954d2f00ba7de5dc0
Mathieu Poussin <mathieu.poussin@cardiologs.com>
2022-11-22 20:26:36 +0100

    initial commit

---
+++ MauiFlyout/MauiFlyout/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using MauiFlyout.ViewModels;
+
+namespace MauiFlyout.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MauiFlyout/MauiFlyout/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MauiFlyout.Models;
+using MauiFlyout.ViewModels;
+
+namespace MauiFlyout.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
